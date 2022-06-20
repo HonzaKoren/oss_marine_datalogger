01dce96294bd36fd4a4336cbf9eaff1905ab3b3c
Jan Kořínek <honza@kekorenum.com>
2022-06-20 14:25:47 +0200

    Prepare initial setup to support all features

--- install.md
+++ install.md
@@ -2,8 +2,8 @@
 
 ```bash
 sudo apt update
-sudo apt install -y git ansible
-git clone https://github.com/HonzaKoren/oss_marine_datalogger.git 
+sudo apt install -y git
+git clone https://github.com/HonzaKoren/oss_marine_datalogger.git
 cd ~/oss_marine_datalogger
 ./install.sh
 ```
--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 # Just to be sure, make a full update
-APT="sudo $(which apt-get)"
+APT="sudo $(which apt)"
 
 $APT update
 $APT upgrade -y
 $APT full-upgrade -y
-$APT dist-upgrade -y
 $APT autoremove -y
 
 # Necessary and usefull tools for running full stack deploy Ansible playbook
