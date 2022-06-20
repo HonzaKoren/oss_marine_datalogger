3a75b58f74e8ed4ef4101cec2c0119931fb5ea8c
Jan Kořínek <honza@kekorenum.com>
2022-06-20 15:36:44 +0200

    Install: Another iteration

--- install.sh
+++ install.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# Check locales and add en_US to default set
+locales=$(grep -v -e "^#" -e "^$" -e "en_GB.UTF-8" /etc/locale.gen | wc -l)
+
+# If there is only a default locale set, add en_US.UTF-8
+if [ $locales -eq 0 ]
+then
+    echo "en_US.UTF-8 UTF-8" | sudo tee -a /etc/locale.gen
+    echo "LANG=en_US.UTF-8" | sudo tee -a /etc/default/locale
+fi
+
+# Re-generate locales
+sudo locale-gen
+
 # Just to be sure, make a full update
 APT="sudo $(which apt)"
 
