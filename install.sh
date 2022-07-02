3c1e1f7a34aaa37a8b9bb17f0ba0b516051e2691
Jan Kořínek <honza@kekorenum.com>
2022-07-02 11:20:13 +0200

    Fix perl locale error messages

--- install.sh
+++ install.sh
@@ -7,7 +7,20 @@ locales=$(grep -v -e "^#" -e "^$" -e "en_GB.UTF-8" /etc/locale.gen | wc -l)
 if [ $locales -eq 0 ]
 then
     echo "en_US.UTF-8 UTF-8" | sudo tee -a /etc/locale.gen
-    echo "LANG=en_US.UTF-8" | sudo tee -a /etc/default/locale
+    echo "LANG=en_US.UTF-8" | sudo tee /etc/default/locale
+    echo "LANGUAGE=en_US.UTF-8" | sudo tee -a /etc/default/locale
+    echo "LC_ALL=en_US.UTF-8" | sudo tee -a /etc/default/locale
+    echo "LC_ADDRESS=en_US.UTF-8" | sudo tee -a /etc/default/locale
+    echo "LC_NAME=en_US.UTF-8" | sudo tee -a /etc/default/locale
+    echo "LC_MONETARY=en_US.UTF-8" | sudo tee -a /etc/default/locale
+    echo "LC_PAPER=en_US.UTF-8" | sudo tee -a /etc/default/locale
+    echo "LC_IDENTIFICATION=en_US.UTF-8" | sudo tee -a /etc/default/locale
+    echo "LC_TELEPHONE=en_US.UTF-8" | sudo tee -a /etc/default/locale
+    echo "LC_MEASUREMENT=en_US.UTF-8" | sudo tee -a /etc/default/locale
+    echo "LC_TIME=en_US.UTF-8" | sudo tee -a /etc/default/locale
+    echo "LC_ALL=en_US.UTF-8" | sudo tee -a /etc/default/locale
+    echo "LC_COLLATE=en_US.UTF-8" | sudo tee -a /etc/default/locale
+    echo "LC_NUMERIC=en_US.UTF-8" | sudo tee -a /etc/default/locale
 fi
 
 # Re-generate locales
